2bd41cdb1ac0c4ff1dc50d8e792eea3d5ed3f4c5
bouhouchhamza <bouhouchhamza52@gmail.com>
2026-02-08 17:01:18 +0100

    feat(routes): add web.php

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\LinksController;
+use App\Http\Controllers\DashboardController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::middleware(['auth','check.active'])->group(function () {
+Route::get('/dashboard', [DashboardController::class, 'index']) 
+    ->name('dashboard');
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('categories',CategoryController::class);
+    Route::resource('links',LinksController::class);
+
+    });
+
+require __DIR__.'/auth.php';
